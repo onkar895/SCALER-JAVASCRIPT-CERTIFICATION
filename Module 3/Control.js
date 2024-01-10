fb7a392af8a212480311d839ec581e54482ca1ea
Onkar Karale <onkarkarale4@gmail.com>
2024-01-10 23:49:56 +0530

    Control Statements

---
+++ Module 3/Control.js
@@ -0,0 +1,13 @@
+// Loops are the statements that we can use to control the flow of the program and to do repetative tasks.
+
+var a = 'Hello World'
+
+for (var i = 0; i <= 10; i++) {
+  console.log(a)
+}
+
+// You have an array and you have to square each element of that array.
+
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (var i = 0; i)
