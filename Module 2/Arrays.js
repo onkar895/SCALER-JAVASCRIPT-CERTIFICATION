4fb0682e58c4012dd3515e26045c28de2bd5e388
Onkar Karale <onkarkarale4@gmail.com>
2024-01-10 16:54:58 +0530

    Basics of JavaScript

---
+++ Module 2/Arrays.js
@@ -0,0 +1,18 @@
+// Arrays provides you an ordered collection of data.
+
+var arr = [12, 15.6, 'onkar', true]
+console.log(arr)
+
+// accesing the elements
+
+var a = arr[2]
+console.log(a)
+
+// replacing elements of an array
+
+arr[2] = 'Manish'
+console.log(arr)
+
+// Getting the Length of an array
+
+console.log('Length of an given array is:', arr.length)
---
+++ Module 2/var.js
@@ -0,0 +1,7 @@
+// There is a reserve keyword in javascript which is used to declare a variables that us 'var' keyword
+
+var a = 20
+console.log(a)
+
+var greet = 'hello'
+console.log(greet)
