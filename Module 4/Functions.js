3bffc3504f11ff11fedf7550a4696fc7bccd7752
Onkar Karale <onkarkarale4@gmail.com>
2024-01-12 00:14:12 +0530

    Functions and Hoisting in JavaScript

--- Module 4/Functions.js
+++ Module 4/Functions.js
@@ -1,7 +1,25 @@
 // A Function is a block of code that performs a specific task.
 
+// Simple Function
 function Greet () {
   console.log('Good Morning')
 }
 
 Greet()
+
+// Passing Parameters and Arguments
+function Add (a, b) {
+  console.log('Addition:', a + b)
+}
+
+Add(4, 5)
+
+// Expressions : It is a set of statements
+
+var add = function (x, y) {
+  // console.log(x + y)
+  return x + y
+}
+
+let result = add(3, 4)
+console.log(result)
