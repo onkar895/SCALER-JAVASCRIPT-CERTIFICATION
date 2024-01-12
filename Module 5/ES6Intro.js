4e2ce23d1fc642f311d73c64f0d9d9fd84128c2d
Onkar Karale <onkarkarale4@gmail.com>
2024-01-12 19:49:06 +0530

    Intro to ES6

---
+++ Module 5/ES6Intro.js
@@ -0,0 +1,16 @@
+// ES6 (EcmaScript version 6) founded in 2015 :
+// ECMA stands for (European Computer Manufacturers Association)
+
+// ES6 comes with significant changes to the javascript langauge.
+// It brought several new features like let and const keyword, rest and spread operators, template literals, classes, modules etc. to make javascript programming easier.
+
+// ES6 Every Feature -
+// 1. let and const keyword
+// 2. Arrow Functions
+// 3. Default Parameters
+// 4. Template Literals and Multi Line Strings
+// 5. Destructuring
+// 6. Higher Order Functionfa
+// 7. Promises
+// 8. Classes
+// 9. Modules
