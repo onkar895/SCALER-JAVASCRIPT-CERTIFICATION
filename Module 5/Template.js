8be9395d839771212f187c0ace456f33a4f07538
Onkar Karale <onkarkarale4@gmail.com>
2024-01-12 23:50:41 +0530

    Template Literals

---
+++ Module 5/Template.js
@@ -0,0 +1,10 @@
+// Template Literals :
+
+let lastName = 'karale'
+console.log(`My name is Onkar ${lastName}`)
+
+// ` ` This sign is called as backticks
+
+let a = 24
+
+console.log(`My age is ${a}`)
