4ccd03489b03dd7d8cd2e41e9bd4ebe56a53eea8
Onkar Karale <onkarkarale4@gmail.com>
2024-01-17 00:03:09 +0530

    Higher Order Functions : map method

--- Module 6/HigherOrderFunc.js
+++ Module 6/HigherOrderFunc.js
@@ -1,9 +1,9 @@
 // Higher Order Function :
 
 // Higher Order Functions are functions that operate on other functions, either by taking them as arguments or by returning them.
-// In simple words, A higher order function is a function that receivees a function as an argument or returns the function as output.
+// In simple words, A higher order function is a function that receives a function as an argument or returns the function as output.
 
-// Normal Way :
+// Normal Way to get Squared array :
 let arr = [1, 2, 3, 4, 5]
 
 let arrSquare = []
@@ -13,7 +13,9 @@ for (let i = 0; i < arr.length; i++) {
 }
 console.log(arrSquare)
 
-// Higher Ordedr Function : Using map
+// Higher Order Function : 
+// Using map method to get Squared array  :
+// Map will loop through eveery element of your array and will perform specific operations that you have provided
 const arr1 = [1, 2, 3, 4, 5]
 
 const SquaredArr = arr1.map((x) => {
@@ -21,3 +23,16 @@ const SquaredArr = arr1.map((x) => {
 })
 
 console.log(SquaredArr)
+
+// Another Example :
+// Convert Rs. into $
+
+const transactions = [1000, 2000, 3000, 5000, 7000, 8000]
+
+const INRTODOLLAR = 80
+
+const transactionInINR = transactions.map((Inr) => {
+  return Inr / INRTODOLLAR
+})
+
+console.log(transactionInINR)
