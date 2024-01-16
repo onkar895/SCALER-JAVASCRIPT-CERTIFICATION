54c24afa4ee0818f4a737cb5c6fe67f0f4e4921f
Onkar Karale <onkarkarale4@gmail.com>
2024-01-16 16:21:37 +0530

    Functional Programming and it's features

---
+++ Module 6/FuncProg.js
@@ -0,0 +1,16 @@
+// Functional Programming :
+
+// Functional Programming is a programming paradigm designed to handle functions in a better way in javascript(mainly pure functions).
+// JavaScript is a multi-paradigm language that we can use OOPS, procedural and functional programming paradigms all at the same time.
+
+// Functional Programming has a few more concepts that you need to know and understand.
+// By implementing those concepts in the application you will end up with more functional code. This will make your code easy to understand and test.
+
+// Key Features in Functional Programming :
+
+// 1. Imperative and Declarative way of writing the code
+// 2. Pure functions and what are the side effects 
+// 3. Higher Order Functions (map, filter and reduce)
+// 4. Composition
+// 5. Closures 
+// 6. Currying
