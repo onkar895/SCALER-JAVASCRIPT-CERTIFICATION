543d4fffbbb3432643783edb16a8160c3b236e7d
Onkar Karale <onkarkarale4@gmail.com>
2024-01-19 23:57:22 +0530

    Object Oriented Programming

--- Module 7/test_strict.js
+++ Module 7/test_strict.js
@@ -1,7 +1,6 @@
-// This Keyword :
-
-// 'this' keyword refers to an object that is executing the current piece of code.
-// It references the object that executing the current function.
+// use strict : 
+// In JavaScript, the "use strict" directive is used to enable a stricter set of rules and error-checking during the execution of code. 
+// When you include "use strict" at the beginning of a script or a function, it changes the way JavaScript behaves by catching common coding errors and preventing the use of certain features that are considered error - prone or deprecated.
 
 'use strict'
 a = 10
