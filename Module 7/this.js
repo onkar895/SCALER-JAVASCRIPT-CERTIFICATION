1f87f82b8146625a7b4f47969b1ce4c44a8545ef
Onkar Karale <onkarkarale4@gmail.com>
2024-01-19 23:52:40 +0530

    Object Oriented Programming

--- Module 7/this.js
+++ Module 7/this.js
@@ -2,3 +2,10 @@
 
 // 'this' keyword refers to an object that is executing the current piece of code.
 // It references the object that executing the current function.
+
+'use strict'
+a = 10
+console.log(a) // error when using strict
+
+let b = 10
+console.log(a)
