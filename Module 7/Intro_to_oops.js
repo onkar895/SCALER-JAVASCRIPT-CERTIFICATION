18b7a78d29fdb87e176b8461a54bb9b88052da73
Onkar Karale <onkarkarale4@gmail.com>
2024-01-19 23:41:43 +0530

    Intro to Object Oriented Programming

---
+++ Module 7/Intro_to_oops.js
@@ -0,0 +1,23 @@
+// Object Oriented JavaScript :
+
+// Object Oriented Programming is a computer programming model that organizes software design around data, or objects, rather than functions and logic.
+// An Objects can be defined as a data field that has unique attributes and behaviour.
+
+// The Four Pillars of OOPS :
+// Encapsulation
+// Abstraction
+// Polymorphism
+// Inheritance
+
+// JavaScript is not actually object oriented language, but we can write object oriented code by using something called as a prototype object.
+
+// OOPS MOdules :
+// 1. This Keyword
+// 2. Constructor functions
+// 3. Classes 
+// 4. Classical Inheritance
+// 5. Encapsulation
+// 6. Polymorphism
+// 7. Prototype Object
+// 8. Prototypal Inheritance
+// 9. Call, Apply, Bind
