5a69f669af7b0139293d0e8abec9413d6624ad0f
Onkar Karale <onkarkarale4@gmail.com>
2024-01-29 23:52:14 +0530

    Object Oriented Programing

---
+++ Module 7/Prototypes/Prototype.js
@@ -0,0 +1,11 @@
+let myObj = {}
+
+console.log(myObj)
+
+let person = {
+  name: 'onkar',
+  age: 25
+}
+
+console.log(person.hasOwnProperty('name')) // true
+console.log(person.hasOwnProperty('salary')) // false
