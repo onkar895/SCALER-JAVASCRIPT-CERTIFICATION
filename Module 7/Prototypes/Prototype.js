29c20321538b0ccc339b82a52fca571023fb6961
Onkar Karale <onkarkarale4@gmail.com>
2024-01-30 00:09:32 +0530

    Object Oriented Programing

--- Module 7/Prototypes/Prototype.js
+++ Module 7/Prototypes/Prototype.js
@@ -9,6 +9,8 @@ let person = {
 // hasOwnProperty actually searches for the particular key in the object person.
 console.log(person.hasOwnProperty('name')) // true
 console.log(person.hasOwnProperty('salary')) // false
+
 // JavaScript has inbuild functions or methods that was alraedy defined for the objects.
 // So whenever you create an object that 'prototype' object will always get linkd to that particular object, so that you can use every one of that properties and methods.
-// So this is what prototype object is.
+// So this is what 'prototype object' is.
+// 'Prototype object' is basically a object that gets link to your object by default and inside that prototype object you will find all the methods that you can use with your particular objects  
