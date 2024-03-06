886312307419bce36f70de3753f66512925ccb6f
Onkar Karale <onkarkarale4@gmail.com>
2024-03-06 15:19:10 +0530

    Asynchronous Programming

---
+++ Module 8/Sync-Async-Intro.js
@@ -0,0 +1,8 @@
+// Synchronous programming:
+// Synchronous programming means that it will use a single thread, single threaded means that javascript can only execute one command at a time.
+// Synchronous is blocking. So that means, it can only go to the next line once the current line has been finished executing.
+
+// Asynchronous JavaScript :
+// Asynchronous JavaScript code is something, which start at current interval and ends later.
+// Asynchronous programming is a way for a computer program to handle multiple tasks simultaneously rather than executing them one after the other.
+// Asynchronous is non-blocking that it will sends multiple requests to the server at the same time.
