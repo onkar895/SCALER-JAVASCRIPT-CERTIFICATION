38a83f8bfc9fbac40a778ab748ebce615e177ac3
Onkar Karale <onkarkarale4@gmail.com>
2024-03-07 02:03:29 +0530

    Asynchronous Programming and coffeShop

--- Module 8/CoffeeShop.js
+++ Module 8/CoffeeShop.js
@@ -1,23 +1,27 @@
-// CoffeeShop :
+// CoffeeShop : We have a coffeShope that only accepts the order for the coffee.
 
+// Recieved the order :
 const PlaceOrder = (drink) => {
   return new Promise((resolve, reject) => {
-    if (drink === 'coffee') {
-      resolve('Order for coffee recieved')
-    } else {
-      reject('Other orders rejected')
-    }
+    setTimeout(() => {
+      if (drink === 'coffee') {
+        resolve('Order for coffee recieved')
+      } else {
+        reject('Other orders rejected')
+      }
+    }, 2000)
   })
 }
 
+// then processed and then served the order :
 const processOrder = (order) => {
   return new Promise((resolve) => {
     setTimeout(() => {
       console.log('Order is being processed')
       setTimeout(() => {
         resolve(`${order} and is Served`)
-      }, 5000)
-    }, 3000)
+      }, 6000)
+    }, 4000)
   })
 }
 
