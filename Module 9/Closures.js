174946ea8107f802105e32a9bb805e15a8acfa70
Onkar Karale <onkarkarale4@gmail.com>
2024-03-07 21:20:14 +0530

    Closures

--- Module 9/Closures.js
+++ Module 9/Closures.js
@@ -20,3 +20,30 @@ fun()
 // So what lexical Scope basically means that the test1() function always have the access to its outer environment which is function test() in this case. So it will have the access to a = 2 also. so output will be 2.
 
 // So we are calling the test1 function but we are actually able to access the 'a' value also. so that is what 'clousure' is.
+
+// Some complex examples of closures :
+
+const greet = () => {
+  let name = 'steve'
+
+  const displayName = () => {
+    console.log('Hi' + ' ' + name)
+
+    let age = 25
+
+    const displayAge = () => {
+      console.log('My age is' + ' ' + age)
+    }
+    return displayAge
+  }
+  return displayName
+}
+
+let g1 = greet()
+console.log(g1)
+
+g1() // displayName
+
+let g2 = g1()
+console.log(g2)
+g2() // displayAge
