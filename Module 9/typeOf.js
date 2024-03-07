6d76b922161a36dc7f7de7d61b0f71c3a2da38bf
Onkar Karale <onkarkarale4@gmail.com>
2024-03-08 01:00:04 +0530

    typeof Operator

---
+++ Module 9/typeOf.js
@@ -0,0 +1,21 @@
+// typeof Operator :
+
+let a = 2
+console.log(typeof (a)) // number
+
+let b = 'scalar'
+console.log(typeof (b)) // string
+
+let c = true
+console.log(typeof (c)) // boolean
+
+let myObj = {
+  name: 'onkar',
+  age: 24
+}
+
+console.log(typeof (myObj)) // object
+
+let arr = [1, 2, 3, 4]
+console.log(typeof (arr)) // object
+// Always remember that in javascript arrays are objects, so it always been return a object
